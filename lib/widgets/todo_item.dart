0f6ede1407ab93c985dc39b5a6a5a341e43d72b2
Artur Hrymalak Llantada <145074109+arturhrymalak1@users.noreply.github.com>
2024-08-05 11:44:43 -0300

    Update todo_item.dart

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -53,7 +53,7 @@ class ToDoItem extends StatelessWidget {
             iconSize: 18,
             icon: Icon(Icons.delete),
             onPressed: () {
-              TaskiiRepository.delete;
+              TaskiiRepository.delete(todo.id!);
             },
           ),
         ),
